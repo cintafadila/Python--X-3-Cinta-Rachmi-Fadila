66600b5036c2ee80bc6bf6fa4d97ae8a72ce476f
Cinta Rachmi Fadila <78463342+cintafadila@users.noreply.github.com>
2021-05-05 13:31:40 +0700

    Tugas Program Python Rekursi

---
+++ rekursi 3.py
@@ -0,0 +1,12 @@
+def penjumlahan(n) :
+    if n <= 1:
+        return n
+    else:
+        return n + penjumlahan(n-1)
+
+bil = int(input('input bilangan : '))
+
+if bil < 0:
+    print('Masukkan bilangan positif')
+else:
+    print('Penjumlahan dari nilai asli',bil,'adalah', penjumlahan(bil))
